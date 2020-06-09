1a28256a3193998507f5885bb32bb775bad3bbdf
toponever <nile21za@hotmail.com>
2020-06-09 09:23:11 +0700

    testtt

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('/test', (req, res) => {
-  return res.send('<p> this is okay </p>')
-})
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-
-app.listen(8080 , () => console.log('This is production ')) ;
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/icon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
