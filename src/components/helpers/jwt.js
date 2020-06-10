29802d3b04d624202004a3ed508a9f37d7e70e0d
toponever <nile21za@hotmail.com>
2020-06-10 11:09:09 +0700

    test route

--- src/components/helpers/jwt.js
+++ src/components/helpers/jwt.js
@@ -1,3 +1,13 @@
-export const getjwt = () => {
+{/* export const getjwt = () => {
     return localStorage.getItem('user')
-}
+} */}
+
+export const getjwt = () => {
+    const user = localStorage.getItem('user')
+  
+    if (user && user.accessToken) {
+      return { Authorization: 'Bearer ' + user.accessToken }
+    } else {
+      return {}
+    }
+  }
