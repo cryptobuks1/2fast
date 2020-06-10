2d765dd49f9140affb54ade59b79c0394f244b68
toponever <nile21za@hotmail.com>
2020-06-10 19:17:30 +0700

    testtttt

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import * as serviceWorker from "./serviceWorker";
 
 //import '@fortawesome/fontawesome-free/css/all.min.css';
 //import 'bootstrap/dist/css/bootstrap.css';
-import 'mdbreact/dist/css/mdb.css';
-import 'semantic-ui-css/semantic.min.css'
+//import 'mdbreact/dist/css/mdb.css';
+//import 'semantic-ui-css/semantic.min.css'
 
 
 ReactDOM.render(
