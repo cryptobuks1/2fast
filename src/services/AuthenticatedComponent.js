aa63fd0232145a13bfaea92d563b2dc8c986c708
toponever <nile21za@hotmail.com>
2020-06-10 13:19:03 +0700

    spinner

--- src/services/AuthenticatedComponent.js
+++ src/services/AuthenticatedComponent.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react'
 import { getjwt } from '../components/helpers/jwt'
 import { withRouter } from "react-router-dom"
 import axios from 'axios'
-import Logo from '../image/newFast.png'
+
+import { css } from "@emotion/core";
+import DotLoader from "react-spinners/DotLoader";
+const override = css`
+position:absolute;
+top:0;
+left:0;
+right:0;
+bottom:0;
+margin:auto;
+`;
 
 class AuthenticatedComponent extends Component {
     constructor(props){
@@ -32,7 +42,16 @@ class AuthenticatedComponent extends Component {
         if(this.state.user === undefined){
             return (
                 <div>
-                   Loading ...
+                <div style={{  
+                    height:' 100%',
+                    width:'100%' }}>
+                  <DotLoader
+                  css={override}
+                  size={100}
+                  color={"#E8DA10"}
+            
+                />
+                </div>
                 </div>
             )
         }
